ff73c1af06b1a5345ec33e22cb41c8fadd1bc29d
Alex Bhusal <alexbhusal@icloud.com>
2026-02-16 21:59:40 +0545

    feat(utils): add map types and constants

---
+++ src/utils/CustomMap.tsx
@@ -0,0 +1,61 @@
+export const customMapStyle = [
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
+
+export const nepalInitialRegion = {
+  latitude: 27.7172, 
+  longitude: 85.3240,     
+  latitudeDelta: 0.5,
+  longitudeDelta: 0.5,
+}
